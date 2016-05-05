305f33b3af841c405098f1c5f14535781d7867f2
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2016-05-06 00:16:38 +0530

    Initial schema

---
+++ handlers.go
@@ -0,0 +1,21 @@
+package shadowfax
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+)
+
+// Env Functionalities required by the entire Application
+type Env struct {
+	Db *sql.DB
+}
+
+// RegisterHandlers adds the route handlers for various calls
+func RegisterHandlers(router *httprouter.Router, env Env) {
+
+}
